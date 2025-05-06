df7b99768bd95aae52eb6c8f410ae08e2477be3d
Gabriel Sarubi <105673747+GabrielSarubi-7@users.noreply.github.com>
2025-05-06 20:39:57 -0300

    Atualização BD
    Atualização no nome da Tabela para "cliente"

--- dbclientes.sql
+++ dbclientes.sql
@@ -2,12 +2,13 @@ CREATE DATABASE dbcliente;
 
 USE dbcliente;
 
-CREATE TABLE clientes (
+CREATE TABLE cliente (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(11) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
-    datanascimento DATE NOT NULL,
+    datanascimento DATE NOT NULL
 );
+select * from cliente;
